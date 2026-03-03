45c8788a95624086cf795d800ff1bb67b266561c
devpolas <195308698+devpolas@users.noreply.github.com>
2026-03-03 17:01:31 +0600

    simple express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.listen(8080, () => {
+  console.log("server is running!");
+});
